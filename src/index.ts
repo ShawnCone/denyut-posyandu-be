907e2a313d8d92f0acf69b7494e63286ea86e0ff
ShawnCone <deandre.dave@gmail.com>
2023-12-28 17:26:56 -0800

    basic graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,83 @@
+// npm install @apollo/server express graphql cors
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import cors from 'cors'
 import express from 'express'
+import http from 'http'
 
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`
+
+interface MyContext {
+  token?: string
+}
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+]
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+}
+
+// Required logic for integrating with Express
 const app = express()
-const port = 8000
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app)
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
+const server = new ApolloServer<MyContext>({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+})
+// Ensure we wait for our server to start
+await server.start()
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/graphql',
+  cors<cors.CorsRequest>(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+)
+
+app.use('/healthcheck', (_req, res) => {
+  res.status(200).send('OK')
 })
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+// Modified server startup
+await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve))
+console.log(`🚀 Server ready at http://localhost:4000/`)
